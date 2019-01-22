1a878c928922f963e9b8c3492875ee5aab34e0d3
Hao Xu <xuh@cs.unc.edu>
2019-01-22 16:55:36 -0500

    update

---
+++ module1.py
@@ -0,0 +1,49 @@
+from icees import iceesclient
+
+def question1(input_cohort_id, maximum_p_value):
+    AssociationToAllFeaturesObject = iceesclient.AssociationToAllFeatures()
+    assoc_to_all_features = AssociationToAllFeaturesObject.run_association_to_all_features('TotalEDInpatientVisits','<', '2', str(maximum_p_value), input_cohort_id)
+    features = []
+    for assoc in assoc_to_all_features["return value"]:
+        feature_b = assoc["feature_b"]
+        feature_name_b = feature_b["feature_name"]
+        feature_bins_b = list(map(lambda x : x["value"], feature_b["feature_qualifiers"]))
+        chi_squared = assoc["chi_squared"]
+        p_value = assoc["p_value"]
+        counts0 = list(map(lambda x: x[0]["frequency"], assoc["feature_matrix"]))
+        counts1 = list(map(lambda x: x[1]["frequency"], assoc["feature_matrix"]))
+        features.append({
+            "name": feature_name_b,
+            "bins": feature_bins_b,
+            "counts_by_bin_0": counts0,
+            "counts_by_bin_1": counts1,
+            "chi_squared": chi_squared,
+            "p_value": p_value
+        })
+
+    return features
+
+def question2(input_cohort_id, maximum_p_value):
+    AssociationToAllFeaturesObject = iceesclient.AssociationToAllFeatures()
+    assoc_to_all_features = AssociationToAllFeaturesObject.run_association_to_all_features('EstResidentialDensity','<', '2', str(maximum_p_value), input_cohort_id)
+    features = []
+    for assoc in assoc_to_all_features["return value"]:
+        feature_b = assoc["feature_b"]
+        feature_name_b = feature_b["feature_name"]
+        feature_bins_b = list(map(lambda x : x["value"], feature_b["feature_qualifiers"]))
+        chi_squared = assoc["chi_squared"]
+        p_value = assoc["p_value"]
+        counts0 = list(map(lambda x: x[0]["frequency"], assoc["feature_matrix"]))
+        counts1 = list(map(lambda x: x[1]["frequency"], assoc["feature_matrix"]))
+        features.append({
+            "name": feature_name_b,
+            "bins": feature_bins_b,
+            "counts_by_bin_0": counts0,
+            "counts_by_bin_1": counts1,
+            "chi_squared": chi_squared,
+            "p_value": p_value
+        })
+
+    return features
+
+
